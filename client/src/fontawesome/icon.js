0492ee0247889c10283625b8571a3dc0b0df1db8
frostedCupcake <99611801+frostedCupcake@users.noreply.github.com>
2022-07-24 22:13:42 +0530

    Delete client directory

--- client/src/fontawesome/icon.js
+++
@@ -1,5 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelope, faCalendar, faPhone, faLocationDot} from '@fortawesome/free-solid-svg-icons'
-
-library.add(faEnvelope, faCalendar, faPhone, faLocationDot)
-
