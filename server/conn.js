404436011aa8de8d068c91e915777ec9eab8995c
Suryaansh Jain <91973466+srynsh@users.noreply.github.com>
2022-07-26 10:13:13 +0530

    Update conn.js

--- server/conn.js
+++ server/conn.js
@@ -1,8 +1,11 @@
 import { Sequelize } from "sequelize";
 
-const userName = "root";
-const password = "Tsunami123!";
-const dbName = "milan";
+// MySQL has to be installed and running for this to work.
+// A database with `${dbName}` has to be manually made. Tables are made automatically.
+
+const userName = "<user name>";
+const password = "<password>";
+const dbName = "<database name>";
 
 const db = new Sequelize(`${dbName}`, `${userName}`, `${password}`, {
   host: "localhost",
