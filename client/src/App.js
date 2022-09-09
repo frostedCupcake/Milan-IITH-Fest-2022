660b572e5b561c2a5483c3336ececf6c9ab4a6f1
jaswanth <jaswanthbeere@gmail.com>
2022-09-09 17:26:37 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
